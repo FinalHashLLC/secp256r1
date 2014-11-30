5a0f06084a4af1a8cb598afc2a0488213923506c
Pieter Wuille <pieter.wuille@gmail.com>
2014-11-30 18:09:02 +0100

    Rewrite squaring assembly to use Peter Dettman's parallel version

--- src/field_5x52_asm.asm
+++ src/field_5x52_asm.asm
@@ -225,8 +225,6 @@ SYM(secp256k1_fe_mul_inner):
 
 	mov rsi,r8		; load c into t9 and destroy b.n[4]
 
-	;; *******************************************************
-common_exit_norm:
 	mov rdi,01000003D10h	; load constant
 
 	mov rax,r15		; get t5
@@ -311,16 +309,14 @@ common_exit_norm:
 	;;  PROC ExSetSquare
 	;;  Register Layout:
 	;;  INPUT: 	rdi	 = a.n
-	;; 	   	rsi  	 = this.a
+	;; 	   	rsi  	 = r.n
 	;;  INTERNAL:	rdx:rax  = multiplication accumulator
 	;; 		r9:r8    = c
-	;; 		r10-r13  = t0-t3
-	;; 		r14	 = a.n[0] / t4
-	;; 		r15	 = a.n[1] / t5
-	;; 		rbx	 = a.n[2] / t6
-	;; 		rcx	 = a.n[3] / t7
-	;; 		rbp	 = 0FFFFFFFFFFFFFh / t8
-	;; 		rsi	 = a.n[4] / t9
+	;;		r10:r14  = a0-a4
+	;;		rcx:rbx  = d
+	;; 		rbp	 = R
+	;; 		rdi	 = t?
+	;;		r15	 = M
 	GLOBAL SYM(secp256k1_fe_sqr_inner)
 	ALIGN 32
 SYM(secp256k1_fe_sqr_inner):
@@ -330,140 +326,204 @@ SYM(secp256k1_fe_sqr_inner):
 	push r13
 	push r14
 	push r15
-	push rsi
-	mov rbp,0FFFFFFFFFFFFFh
-	
-	;; c=a.n[0] * a.n[0]
-   	mov r14,[rdi+0*8]	; r14=a.n[0]
-	mov r10,rbp		; modulus 
+	mov r10,[rdi+0*8]
+	mov r11,[rdi+1*8]
+	mov r12,[rdi+2*8]
+	mov r13,[rdi+3*8]
+	mov r14,[rdi+4*8]
+	mov rbp,01000003D10h
+	mov r15,0fffffffffffffh
+
+	;; d = (a0*2) * a3
+	lea rax,[r10*2]
+	mul r13
+	mov rbx,rax
+	mov rcx,rdx
+	;; d += (a1*2) * a2
+	lea rax,[r11*2]
+	mul r12
+	add rbx,rax
+	adc rcx,rdx
+	;; c = [4]*[4]
 	mov rax,r14
-	mul rax
-	mov r15,[rdi+1*8]	; a.n[1]
-	add r14,r14		; r14=2*a.n[0]
+	mul r14
 	mov r8,rax
-	and r10,rax		; only need lower qword
-	shrd r8,rdx,52
-	xor r9,r9
-
-	;; c+=2*a.n[0] * a.n[1]
-	mov rax,r14		; r14=2*a.n[0]
-	mul r15
-	mov rbx,[rdi+2*8]	; rbx=a.n[2]
-	mov r11,rbp 		; modulus
-	add r8,rax
-	adc r9,rdx
-	and r11,r8
+	mov r9,rdx
+	;; d += (c & M) * R
+	and rax,r15
+	mul rbp
+	add rbx,rax
+	adc rcx,rdx
+	;; c >>= 52 (r8 only)
 	shrd r8,r9,52
-	xor r9,r9
-	
-	;; c+=2*a.n[0]*a.n[2]+a.n[1]*a.n[1]
-	mov rax,r14
-	mul rbx
+	;; t3 (stack) = d & M
+	mov rdi,rbx
+	and rdi,r15
+	push rdi
+	;; d >>= 52
+	shrd rbx,rcx,52
+	mov rcx,0
+	;; a4 *= 2
+	add r14,r14
+	;; d += a0 * a4
+	mov rax,r10
+	mul r14
+	add rbx,rax
+	adc rcx,rdx
+	;; d+= (a1*2) * a3
+	lea rax,[r11*2]
+	mul r13
+	add rbx,rax
+	adc rcx,rdx
+	;; d += a2 * a2
+	mov rax,r12
+	mul r12
+	add rbx,rax
+	adc rcx,rdx
+	;; d += c * R
+	mov rax,r8
+	mul rbp
+	add rbx,rax
+	adc rcx,rdx
+	;; t4 = d & M (rdi)
+	mov rdi,rbx
+	and rdi,r15
+	;; d >>= 52
+	shrd rbx,rcx,52
+	mov rcx,0
+	;; tx = t4 >> 48 (rbp, overwrites constant)
+	mov rbp,rdi
+	shr rbp,48
+	;; t4 &= (M >> 4) (stack)
+	mov rax,0ffffffffffffh
+	and rdi,rax
+	push rdi
+	;; c = a0 * a0
+	mov rax,r10
+	mul r10
+	mov r8,rax
+	mov r9,rdx
+	;; d += a1 * a4
+	mov rax,r11
+	mul r14
+	add rbx,rax
+	adc rcx,rdx
+	;; d += (a2*2) * a3
+	lea rax,[r12*2]
+	mul r13
+	add rbx,rax
+	adc rcx,rdx
+	;; u0 = d & M (rdi)
+	mov rdi,rbx
+	and rdi,r15
+	;; d >>= 52
+	shrd rbx,rcx,52
+	mov rcx,0
+	;; u0 = (u0 << 4) | tx (rdi)
+	shl rdi,4
+	or rdi,rbp
+	;; c += u0 * (R >> 4)
+	mov rax,01000003D1h
+	mul rdi
 	add r8,rax
 	adc r9,rdx
-
-	mov rax,r15
-	mov r12,rbp		; modulus
-	mul rax
-	mov rcx,[rdi+3*8]	; rcx=a.n[3]
-	add r15,r15		; r15=a.n[1]*2
-	add r8,rax
-	adc r9,rdx
-	and r12,r8		; only need lower dword
+	;; r[0] = c & M
+	mov rax,r8
+	and rax,r15
+	mov [rsi+0*8],rax
+	;; c >>= 52
 	shrd r8,r9,52
-	xor r9,r9		
-
-	;; c+=2*a.n[0]*a.n[3]+2*a.n[1]*a.n[2]
-	mov rax,r14
-	mul rcx
+	mov r9,0
+	;; a0 *= 2
+	add r10,r10
+	;; c += a0 * a1
+	mov rax,r10
+	mul r11
 	add r8,rax
 	adc r9,rdx
-
-	mov rax,r15		; rax=2*a.n[1]
-	mov r13,rbp		; modulus
-	mul rbx
-	mov rsi,[rdi+4*8]	; rsi=a.n[4]
-	add r8,rax
-	adc r9,rdx
-	and r13,r8
-	shrd r8,r9,52
-	xor r9,r9		
-
-	;; c+=2*a.n[0]*a.n[4]+2*a.n[1]*a.n[3]+a.n[2]*a.n[2]
-	mov rax,r14		; last time we need 2*a.n[0]
-	mul rsi
-	add r8,rax
-	adc r9,rdx
-
-	mov rax,r15
-	mul rcx
-	mov r14,rbp		; modulus
-	add r8,rax
-	adc r9,rdx
-
+	;; d += a2 * a4
+	mov rax,r12
+	mul r14
+	add rbx,rax
+	adc rcx,rdx
+	;; d += a3 * a3
+	mov rax,r13
+	mul r13
+	add rbx,rax
+	adc rcx,rdx
+	;; load R in rbp
+	mov rbp,01000003D10h
+	;; c += (d & M) * R
 	mov rax,rbx
-	mul rax
-	add rbx,rbx		; rcx=2*a.n[2]
+	and rax,r15
+	mul rbp
 	add r8,rax
 	adc r9,rdx
-	and r14,r8
+	;; d >>= 52
+	shrd rbx,rcx,52
+	mov rcx,0
+	;; r[1] = c & M
+	mov rax,r8
+	and rax,r15
+	mov [rsi+8*1],rax
+	;; c >>= 52
 	shrd r8,r9,52
-	xor r9,r9		
-
-	;; c+=2*a.n[1]*a.n[4]+2*a.n[2]*a.n[3]
-	mov rax,r15		; last time we need 2*a.n[1]
-	mul rsi
+	mov r9,0
+	;; c += a0 * a2 (last use of r10)
+	mov rax,r10
+	mul r12
 	add r8,rax
 	adc r9,rdx
-
+	;; fetch t3 (r10, overwrites a0),t4 (rdi)
+	pop rdi
+	pop r10
+	;; c += a1 * a1
+	mov rax,r11
+	mul r11
+	add r8,rax
+	adc r9,rdx
+	;; d += a3 * a4
+	mov rax,r13
+	mul r14
+	add rbx,rax
+	adc rcx,rdx
+	;; c += (d & M) * R
 	mov rax,rbx
-	mul rcx
-	mov r15,rbp		; modulus
+	and rax,r15
+	mul rbp
 	add r8,rax
 	adc r9,rdx
-	and r15,r8
+	;; d >>= 52 (rbx only)
+	shrd rbx,rcx,52
+	;; r[2] = c & M
+	mov rax,r8
+	and rax,r15
+	mov [rsi+2*8],rax
+	;; c >>= 52
 	shrd r8,r9,52
-	xor r9,r9		
-
-	;; c+=2*a.n[2]*a.n[4]+a.n[3]*a.n[3]
-	mov rax,rbx		; last time we need 2*a.n[2]
-	mul rsi
+	mov r9,0
+	;; c += t3
+	add r8,r10
+	;; c += d * R
+	mov rax,rbx
+	mul rbp
 	add r8,rax
 	adc r9,rdx
-
-	mov rax,rcx		; a.n[3]
-	mul rax
-	mov rbx,rbp		; modulus
-	add r8,rax
-	adc r9,rdx
-	and rbx,r8		; only need lower dword
-	lea rax,[2*rcx]
+	;; r[3] = c & M
+	mov rax,r8
+	and rax,r15
+	mov [rsi+3*8],rax
+	;; c >>= 52 (r8 only)
 	shrd r8,r9,52
-	xor r9,r9		
+	;; c += t4 (r8 only)
+	add r8,rdi
+	;; r[4] = c
+	mov [rsi+4*8],r8
 
-	;; c+=2*a.n[3]*a.n[4]
-	mul rsi
-	mov rcx,rbp		; modulus
-	add r8,rax
-	adc r9,rdx
-	and rcx,r8		; only need lower dword
-	shrd r8,r9,52
-	xor r9,r9		
-
-	;; c+=a.n[4]*a.n[4]
-	mov rax,rsi
-	mul rax
-	;; mov rbp,rbp		; modulus is already there!
-	add r8,rax
-	adc r9,rdx
-	and rbp,r8 
-	shrd r8,r9,52
-	xor r9,r9		
-
-	mov rsi,r8
-
-	;; *******************************************************
-	jmp common_exit_norm
-	end
-
-	
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rbx
+	pop rbp
+	ret
